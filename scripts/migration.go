b6fca97564e663fd037d4a46a6f35fea129d35b5
joe-black-jb <joeblack3601@gmail.com>
2024-08-16 22:52:49 +0900

    マイグレーション時のデータ修正

--- scripts/migration.go
+++ scripts/migration.go
@@ -24,13 +24,54 @@ var Users = []internal.User{
 var Places = []internal.Place{
 	{
 		Name: "カフェ・ベローチェ 京成船橋駅前店",
+		Address: "〒273-0005 千葉県船橋市本町４丁目４４−２５ ルネライラタワー船橋 １Ｆ",
 		Latitude: 35.69985840379561, 
 		Longitude: 139.98670535289517,
 	},
 	{
 		Name: "ドトールコーヒーショップ 船橋駅南口店",
+		Address: "〒273-0005 千葉県船橋市本町１丁目３−１ 船橋FACE 1F",
 		Latitude: 35.70107504987384,
 		Longitude: 139.98581438173127,
+	},
+}
+
+var Stations = []internal.Station{
+	{
+		Name: "船橋",
+		Latitude: 35.70188004983369,
+		Longitude: 139.98537255674557,
+	},
+	{
+		Name: "東京",
+		Latitude: 35.681436611951135,
+		Longitude:  139.76708188176897,
+	},
+	{
+		Name: "新宿",
+		Latitude: 35.69048197271755,
+		Longitude: 139.7004371455568,
+	},
+	{
+		Name: "新船橋",
+		Latitude: 35.71185474592088,
+		Longitude: 139.97975498280078,
+	},
+	{
+		Name: "渋谷",
+		Latitude: 35.6581995011312,
+		Longitude: 139.7016143394409,
+	},
+	{
+		Name: "秋葉原",
+		Latitude: 35.698513736755125,
+		Longitude: 139.77304408861374,
+	},
+	{
+		Name: "東中山",
+	},
+	{
+		Name: "浅草駅",
 	},
 }
 
@@ -55,13 +96,15 @@ func main() {
 	// Drop Table
 	db.Migrator().DropTable(&internal.User{})
 	db.Migrator().DropTable(&internal.Place{})
+	db.Migrator().DropTable(&internal.Station{})
 
 	// Migrate the schema
-	db.AutoMigrate(&internal.User{}, &internal.Place{})
+	db.AutoMigrate(&internal.User{}, &internal.Place{}, &internal.Station{})
 
 	// Batch Create
 	db.Create(&Users)
 	db.Create(&Places)
+	db.Create(&Stations)
 
 	mysql, _ := db.DB()
 	mysql.Close()
