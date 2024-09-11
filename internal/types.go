1a30c25b2600325bae59ce2a28e030cdc78fab35
joe-black-jb <joeblack3601@gmail.com>
2024-09-11 11:24:07 +0900

    struct のフィールドに dynamo 用のタグを追加

--- internal/types.go
+++ internal/types.go
@@ -24,22 +24,22 @@ type User struct {
 
 type Place struct {
 	// gorm.Model
-	ID            string    `gorm:"primaryKey" json:"id"`
-	CreatedAt     time.Time `json:"createdAt"`
-	UpdatedAt     time.Time `json:"updatedAt"`
-	Name          string    `gorm:"unique" json:"name"`
-	Address       string    `json:"address"`
-	Latitude      float64   `json:"latitude"`
-	Longitude     float64   `json:"longitude"`
-	Image         string    `json:"image"`
-	BusinessHours string    `json:"businessHours"`
-	Tel           string    `json:"tel"`
-	Url           string    `json:"url"`
-	Memo          string    `json:"memo"`
-	Socket        int       `json:"socket"`
-	SocketNum     int       `json:"socketNum"`
-	Wifi          int       `json:"wifi"`
-	Smoke         int       `json:"smoke"`
+	ID            string    `gorm:"primaryKey" json:"id" dynamodbav:"id"`
+	CreatedAt     time.Time `json:"createdAt" dynamodbav:"createdAt"`
+	UpdatedAt     time.Time `json:"updatedAt" dynamodbav:"updatedAt"`
+	Name          string    `gorm:"unique" json:"name" dynamodbav:"name"`
+	Address       string    `json:"address" dynamodbav:"address"`
+	Latitude      float64   `json:"latitude" dynamodbav:"latitude"`
+	Longitude     float64   `json:"longitude" dynamodbav:"longitude"`
+	Image         string    `json:"image" dynamodbav:"image"`
+	BusinessHours string    `json:"businessHours" dynamodbav:"businessHours"`
+	Tel           string    `json:"tel" dynamodbav:"tel"`
+	Url           string    `json:"url" dynamodbav:"url"`
+	Memo          string    `json:"memo" dynamodbav:"memo"`
+	Socket        int       `json:"socket" dynamodbav:"socket"`       // 0: なし, 1: あり, 2: 不明
+	SocketNum     int       `json:"socketNum" dynamodbav:"socketNum"` // 0: なし, 1: あり, 2: 不明
+	Wifi          int       `json:"wifi" dynamodbav:"wifi"`
+	Smoke         int       `json:"smoke" dynamodbav:"smoke"`
 }
 
 type Station struct {
